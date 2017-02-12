1dd7d8ce98988c9cd6f495c1c47632aa69b98f96
Michael Drennen <michaeldrennen74@gmail.com>
2017-02-12 03:35:10 -0700

    Added composer.json file. Added laravel models.

---
+++ src/Console/Initialize.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class Initialize extends Command {
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'geonames:initialize';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Download and insert fresh data from geonames.org';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct() {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle() {
+        //
+    }
+}
---
+++ src/GeonamesServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Providers;
+
+use App\Console\Commands\Download;
+use App\Console\Commands\Initialize;
+use Illuminate\Support\ServiceProvider;
+
+
+class GeonamesServiceProvider extends ServiceProvider {
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot() {
+        // There are a number of tables that need to be created for our Geonames package.
+        // Feel free to modify those migrations to create indexes that are appropriate for your application.
+        $this->loadMigrationsFrom(__DIR__ . '/migrations');
+
+
+        // The defaults are fine, but if you want to optimize this is a good place to start.
+        $this->publishes([__DIR__ . '/config/geonames.php' => config_path('geonames.php'),]);
+
+        // Let's register our commands. These are needed to keep our geonames data up-to-date.
+        if ($this->app->runningInConsole()) {
+            $this->commands([Initialize::class, Download::class,]);
+        }
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register() {
+        //
+    }
+}
