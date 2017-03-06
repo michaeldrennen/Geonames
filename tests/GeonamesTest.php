c5ac7e78879f6acaa55cc569e267527f62101a43
Michael Drennen <michaeldrennen74@gmail.com>
2017-03-06 07:33:11 -0700

    Removed some tests that were copied over from another package.

--- tests/GeonamesTest.php
+++ tests/GeonamesTest.php
@@ -3,23 +3,7 @@ use PHPUnit\Framework\TestCase;
 use MichaelDrennen\Geonames;
 
 class GeonamesTest extends TestCase {
-    public function testEmptyInputString() {
-        $isCusip = CUSIP::isCUSIP('');
-        $this->assertFalse($isCusip);
-    }
-
-    public function testInvalidInputString() {
-        $isCusip = CUSIP::isCUSIP('notValidCusip');
-        $this->assertFalse($isCusip);
-    }
-
-    public function testValidInputString() {
-        $isCusip = CUSIP::isCUSIP('222386AA2');
-        $this->assertTrue($isCusip);
-    }
-
-    public function testValidInputStringWithWhitespace() {
-        $isCusip = CUSIP::isCUSIP(' 222386AA2 ');
-        $this->assertTrue($isCusip);
+    public function testTest() {
+        $this->assertFalse(false);
     }
 }
