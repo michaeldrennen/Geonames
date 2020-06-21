70653085e38bd50e6716bb4396556853f6811431
Michael Drennen <michaeldrennen74@gmail.com>
2020-06-20 20:35:16 -0600

    https://github.com/michaeldrennen/Geonames/issues/29

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -11,17 +11,21 @@ Route::get( '/geonames/search-all', '\MichaelDrennen\Geonames\Controllers\Geonam
 Route::get( '/geonames/{term}', '\MichaelDrennen\Geonames\Controllers\GeonamesController@test' );
 
 
-
 Route::get( '/geonames/cities/{asciiNameTerm}', '\MichaelDrennen\Geonames\Controllers\GeonamesController@citiesUsingLocale' );
 
 Route::get( '/geonames/{countryCode}/cities/{asciiNameTerm}', '\MichaelDrennen\Geonames\Controllers\GeonamesController@citiesByCountryCode' );
 
 Route::get( '/geonames/{countryCode}/schools/{asciiNameTerm}', '\MichaelDrennen\Geonames\Controllers\GeonamesController@schoolsByCountryCode' );
 
-Route::get( '/geonames/examples/vue/element/autocomplete', function () {
-    return view( 'geonames::vue-element-example' );
-} );
 
-Route::get( '/geonames/examples/jquery/autocomplete', function () {
-    return view( 'geonames::jquery-example' );
-} );
+/**
+ * Uncomment these, but you will not be able to cache your routes as referenced below:
+ * @url https://laravel.com/docs/7.x/deployment#optimizing-route-loading
+ */
+//Route::get( '/geonames/examples/vue/element/autocomplete', function () {
+//    return view( 'geonames::vue-element-example' );
+//} );
+
+//Route::get( '/geonames/examples/jquery/autocomplete', function () {
+//    return view( 'geonames::jquery-example' );
+//} );
